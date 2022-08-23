5683cac13ca2cc062ebf4dfe079e17ab089c4635
adnan <adnanfahedhr@gmail.com>
2022-08-23 21:44:26 +0300

    init starting the quiz

--- src/features/quiz/comps/Quiz.tsx
+++ src/features/quiz/comps/Quiz.tsx
@@ -1,3 +1,28 @@
+import { useDispatch, useSelector } from "react-redux"
+import { useAppDispatch } from "../../../app/hooks"
+import { anyQuestionIsSelecteed, setToNextQuestion } from "../quizSlice"
+
 export default () => {
+    const anyIsSelected = useSelector(anyQuestionIsSelecteed)
+
+    if (!anyIsSelected) return NotStartedQuiz()
+
     return (<></>)
+}
+
+function NotStartedQuiz() {
+    const dispatch = useAppDispatch()
+
+    const startQuiz = () => dispatch(setToNextQuestion())
+
+    return (
+        <div>
+            <span>
+                Welcome to the Friends Quiz
+            </span>
+            <div>
+                <button onClick={startQuiz}>lets get Started!</button>
+            </div>
+        </div>
+    )
 }
--- src/features/quiz/quizSlice.ts
+++ src/features/quiz/quizSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit"
-import { RootState } from "../../app/store"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import assert from "assert"
+import { AppThunk, RootState } from "../../app/store"
 
 import questions from "../../assets/questions.json"
 
@@ -38,19 +39,57 @@ const isQuizMultiSelection = (que: Question) => que.correctAnswersIds.length > 1
 
 const isAnswerCorrect = (ans: QuestionAnswer, que: Question) => que.correctAnswersIds.includes(ans.id)
 
+const isFirstQuestion = (que: Question, state: RootState) => {
+    let fQ = selectFirstQuestion(state)
+
+    if (fQ == undefined) {
+        throw new Error("There is no first question")
+    }
+
+    return que.id == fQ.id
+}
+
+const isLastQuestion = (que: Question, state: RootState) => {
+    let lQ = selectLastQuestion(state)
+
+    if (lQ == undefined) {
+        throw new Error("There is no last question")
+    }
+
+    return que.id == lQ.id
+}
+
 export const quizSlice = createSlice(
     {
         name: 'quiz',
         initialState,
         reducers: {
-
+            setSelectedQuestion: (state, action: PayloadAction<string>) => {
+                state.selectedQuestionId = action.payload
+            }
         }
     }
 )
 
+export const anyQuestionIsSelecteed = (state: RootState) => state.quiz.selectedQuestionId != null
+
+export const selectCurrentQuestionId = (state: RootState) => state.quiz.selectedQuestionId
+
+export const selectFirstQuestion = (state: RootState) => state.quiz.questions.at(0)
+
+export const selectLastQuestion = (state: RootState) => {
+    const q = state.quiz
+    const ques = q.questions
+
+    const len = ques.length
+
+    return ques.at(len - 1)
+}
+
+
 export const selectCurrentQuestion = (state: RootState) => {
     const quz = state.quiz
-    const slctd = quz.selectedQuestionId
+    const slctd = selectCurrentQuestionId(state)
 
     if (slctd == null) {
         return null
@@ -59,5 +98,20 @@ export const selectCurrentQuestion = (state: RootState) => {
     return quz.questions.find(e => e.id == slctd)
 }
 
+export const { setSelectedQuestion } = quizSlice.actions
+
+export const setToNextQuestion = (): AppThunk =>
+    (dispatch, getState) => {
+        const currentValue = selectCurrentQuestionId(getState()) || "1";
+
+        try {
+            const intId: number = +currentValue
+
+            dispatch(setSelectedQuestion(String(intId + 1)))
+
+        } catch (error) {
+            throw new Error("Ur id should be int convertible!!!");
+        }
+    };
 
 export default quizSlice.reducer;
